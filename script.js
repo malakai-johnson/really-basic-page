7866826ce8e866ac40628f9dc251dd4a562c63ab
malakai-johnson <malakai.johnson642@gmail.com>
2022-01-28 14:22:40 -0800

    add firebaseConfig

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-// console.log('hello!');
-//
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-//
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBiRpstFjmhFNJgKK3i9QCCHSZNH5jXJZ8',
-//   authDomain: 'really-basic-page.firebaseapp.com',
-//   projectId: 'really-basic-page',
-//   storageBucket: 'really-basic-page.appspot.com',
-//   messagingSenderId: '706817634757',
-//   appId: '1:706817634757:web:26a66a21c1d7857afb1bac',
-// };
-//
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+console.log('hello!');
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMl4y0GIGdkt9BJhshrzgPDgepcS0rYqM",
+  authDomain: "really-basic-page-2.firebaseapp.com",
+  projectId: "really-basic-page-2",
+  storageBucket: "really-basic-page-2.appspot.com",
+  messagingSenderId: "1069179269141",
+  appId: "1:1069179269141:web:07a0490501e1e0fc2a5e1c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
