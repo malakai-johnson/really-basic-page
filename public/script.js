ec091d24ef58186c319e6f33ccbd3acb0106d82d
malakai-johnson <malakai.johnson642@gmail.com>
2022-01-28 14:37:02 -0800

    Add JS back

---
+++ public/script.js
@@ -0,0 +1,19 @@
+console.log('hello!');
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMl4y0GIGdkt9BJhshrzgPDgepcS0rYqM",
+  authDomain: "really-basic-page-2.firebaseapp.com",
+  projectId: "really-basic-page-2",
+  storageBucket: "really-basic-page-2.appspot.com",
+  messagingSenderId: "1069179269141",
+  appId: "1:1069179269141:web:07a0490501e1e0fc2a5e1c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
